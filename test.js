51f23954f368319d69b66c9c384bdce509ca5701
Kaze <101078139+KazeDevID@users.noreply.github.com>
2024-07-07 16:37:53 +0800

    Delete test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-const test = () => new Promise(resolve => setTimeout(() => console.log(true), 2000))
-const cmd = "await test()"
-const res = await eval(`(async () => { ${cmd} })()`)
-console.log(res)
